f15544cd26ac109a98680cb8c8c6c9cac6cf4c24
barcar <barcar@users.noreply.github.com>
2016-04-20 16:32:50 +0100

    Adding new diff-log.sh script

---
+++ diff-log.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ROUTER_PASSWORD=$1
+DOWNLOAD_SCRIPT=./download-log.sh
+LOG_FILE=./bthub.log
+TMP_FILE=/tmp/bthub_old.log
+DIFF_FILE=./bthub_diff.log
+
+trap cleanUp EXIT SIGINT SIGTERM
+
+function cleanUp() {
+  [ -f ${TMP_FILE} ] && rm -f ${TMP_FILE}
+}
+
+function diffLog() {
+
+  # Move last downloaded log (if it exists) to temp location
+  [ -f ${LOG_FILE} ] && mv ${LOG_FILE} ${TMP_FILE}
+
+  # Do new log download using external script
+  ${DOWNLOAD_SCRIPT} ${ROUTER_PASSWORD}
+
+  if grep 'Admin login successful' ${LOG_FILE} > /dev/null
+  then
+    echo "Starting diff"
+    # Output any new lines since last execution
+    diff --new-file --old-line-format='' --new-line-format='%L' --unchanged-line-format='' ${TMP_FILE} ${LOG_FILE} > ${DIFF_FILE}
+  else
+    echo "Log not found"
+    # Create an empty diff file if nothing downloaded
+    :> ${DIFF_FILE}
+    exit 1
+  fi
+}
+
+diffLog
